c1c3171cd6f6ca36460818297b820db6849b35ff
nakano16180 <nakano16180@gmail.com>
2020-05-31 10:54:20 +0900

    update doc_manage

--- doc_manage.py
+++ doc_manage.py
@@ -3,6 +3,23 @@ import datetime
 import os
 import shutil
 
+def read_data(data_path):
+    if not os.path.exists(data_path):
+        print("Can't read data. please check data name.")
+    else:
+        with open(data_path, "r") as file:
+            data = file.readlines()
+            pprint.pprint(data)
+            return data
+
+def read_tag(data):
+    for data_line in data:
+        if '### ' in data_line:
+            print('tag3', data_line)
+        elif '## ' in data_line:
+            print('tag2', data_line)
+        elif '# ' in data_line:
+            print('tag1', data_line)
 
 def new_document():
     date = datetime.datetime.now().strftime('%Y-%m-%d %H%M%S')
